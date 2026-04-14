53420a0f582c4888ca60f11947f1e145d8f8b53a
Gagarina Ira <iraache@yandex.ru>
2026-04-15 01:22:34 +0900

    админка

--- create_admin.py
+++ create_admin.py
@@ -7,7 +7,7 @@ django.setup()
 from users.models import CustomUser
 
 if not CustomUser.objects.filter(username='admin').exists():
-    CustomUser.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-    print("Admin created: admin / admin123")
+    CustomUser.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print("Admin created: admin / admin")
 else:
     print("Admin already exists")
