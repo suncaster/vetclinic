0d54addffb7674c84f8f48527e0fdac99b89543f
Gagarina Ira <iraache@yandex.ru>
2026-04-15 01:31:25 +0900

    админка2

--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.exceptions import ValidationError
-
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
@@ -12,20 +10,9 @@ class CustomUser(AbstractUser):
     ]
 
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='CLIENT')
-    phone = models.CharField(max_length=20, blank=True, unique=True, verbose_name="Телефон")
-    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=20, blank=True, verbose_name="Телефон")  # Убрали unique=True
+    email = models.EmailField(verbose_name="Email", blank=True)  # Добавили blank=True
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-
-    def clean(self):
-        # Проверка на уникальность email при создании
-        if CustomUser.objects.filter(email=self.email).exclude(id=self.id).exists():
-            raise ValidationError({'email': 'Пользователь с таким email уже существует'})
-        if self.phone and CustomUser.objects.filter(phone=self.phone).exclude(id=self.id).exists():
-            raise ValidationError({'phone': 'Пользователь с таким номером телефона уже существует'})
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
